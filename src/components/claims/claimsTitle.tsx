88e9dd591ccf994fda1a28bcbe9dc4c377ec7d05
Harut <67683447+HarutQanaqaryan@users.noreply.github.com>
2022-06-22 19:22:51 +0300

    Delete claimsTitle.tsx

--- src/components/claims/claimsTitle.tsx
+++
@@ -1,7 +0,0 @@
-import "../../styles/claims.scss";
-interface ClaimTitleType {
-  title: string;
-}
-export const ClaimsTitle = (prop: ClaimTitleType) => {
-  return <h4 className="claims-head_title">{prop.title}</h4>;
-};
