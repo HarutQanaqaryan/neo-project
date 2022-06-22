5d751ddb05e2fae9d34f9cb6fb4d6674f8d37aee
Harut <67683447+HarutQanaqaryan@users.noreply.github.com>
2022-06-22 19:21:32 +0300

    Delete loading.tsx

--- src/components/loading.tsx
+++
@@ -1,9 +0,0 @@
-import loadingIcon from "../assets/icons/loading.svg";
-
-export const Loading = () => {
-  return (
-    <div className="claims-block_loading">
-      <img src={loadingIcon} alt="Loading" />
-    </div>
-  );
-};
